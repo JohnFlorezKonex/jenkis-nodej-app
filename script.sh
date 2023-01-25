dd3b964c9bc17891cc795a0ff202367665535c6a
John Florez <john.florez25@gmail.com>
2023-01-25 17:37:03 -0500

    test change values in task definition

--- script.sh
+++ script.sh
@@ -1,6 +1,3 @@
-ROLE_ARN=jq .taskDefinition.executionRoleArn
-echo "ROLE_ARN= " $ROLE_ARN
-
 FAMILY=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.family`
 echo "FAMILY= " $FAMILY
 
@@ -9,7 +6,6 @@ echo "NAME= " $NAME
 
 sed -i "s#BUILD_NUMBER#$IMAGE_TAG#g" task-definition.json
 sed -i "s#IMAGE_REPO#$IMAGE_REPO_NAME#g" task-definition.json
-sed -i "s#ROLE_ARN#$ROLE_ARN#g" task-definition.json
 sed -i "s#FAMILY#$FAMILY#g" task-definition.json
 sed -i "s#NAME#$NAME#g" task-definition.json
 
