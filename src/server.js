189d56c1a2436e42c3339b62ddf47c64fb644e4e
John Florez <john.florez25@gmail.com>
2023-01-27 13:52:20 -0500

    add git ignore

--- src/server.js
+++ src/server.js
@@ -3,11 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-<<<<<<< HEAD
     response.end("Actualizando Despliegue a ECS");
-=======
-    response.end("Nuevo test de Despliegue desde Jenkins Ezequiel");
->>>>>>> 3acccd13791c67c2e90b302eb22dc73b9f4cd141
 
 });
 
