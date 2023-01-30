56f9294e5d7d268e5271b1e89938e560c17921ca
John Florez <john.florez25@gmail.com>
2023-01-30 18:35:14 -0500

    new test

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Prueba Funcional 2 ECS Jenkins 30-01-2023");
+    response.end("Actualizando Despliegue a ECS");
 
 });
 
